8797a52e8c53cfb7efe1d79610e6b350032a8724
Ruben Krueger <ruben1@stanford.edu>
2019-10-17 23:40:00 -0700

    Updates

---
+++ precache-manifest.ed608f4ce0eb86ebf9c9f32d3ab5168a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ffe1efc31095a3924946b3a2ba342ac",
+    "url": "/doggos/index.html"
+  },
+  {
+    "revision": "6f91469c483278501692",
+    "url": "/doggos/static/css/main.b002aed4.chunk.css"
+  },
+  {
+    "revision": "13e5977df4bca9fe5fae",
+    "url": "/doggos/static/js/2.2a8a7900.chunk.js"
+  },
+  {
+    "revision": "6f91469c483278501692",
+    "url": "/doggos/static/js/main.8d9086f9.chunk.js"
+  },
+  {
+    "revision": "9c9000d26d3d219d79e4",
+    "url": "/doggos/static/js/runtime-main.07e4775f.js"
+  },
+  {
+    "revision": "6a8cec3497b2fbba3a948a05c21638af",
+    "url": "/doggos/static/media/logo.6a8cec34.png"
+  }
+]);
